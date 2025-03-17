5433acffd419620e0fa8e1c41d5a950c17036767
vientorepublic <markgod0419@gmail.com>
2025-03-17 11:22:21 +0900

    remove unused templates

--- src/main/java/com/dkim/springproj/springproj/GlobalExceptionHandler.java
+++ src/main/java/com/dkim/springproj/springproj/GlobalExceptionHandler.java
@@ -11,7 +11,7 @@ import com.dkim.springproj.springproj.main.utility.Utility;
 @ControllerAdvice
 public class GlobalExceptionHandler {
   @ExceptionHandler(NoResourceFoundException.class)
-  protected ResponseEntity<ErrorDto> NotFoundException() {
+  private ResponseEntity<ErrorDto> NotFoundException() {
     String now = new Utility().getISOTimestamp();
     ErrorDto res = new ErrorDto(now, 404, "Not Found", "Request path not found.");
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(res);
--- src/main/java/com/dkim/springproj/springproj/main/MainController.java
+++ src/main/java/com/dkim/springproj/springproj/main/MainController.java
@@ -2,13 +2,18 @@ package com.dkim.springproj.springproj.main;
 
 import org.springframework.web.bind.annotation.RestController;
 import com.dkim.springproj.springproj.main.dto.MessageDto;
-
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class MainController {
   @GetMapping("/")
   public MessageDto Main() {
     return new MessageDto("Hello, World!");
+  }
+
+  @GetMapping("/test")
+  public MessageDto Test(@RequestParam String name) {
+    return new MessageDto("Hello, " + name + "!");
   }
 }
